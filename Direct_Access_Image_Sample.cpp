3e05d8d7cdffd47fcc3b9db2f1e8eb339940c090
Alex <alex.dan.tomescu@gmail.com>
2013-12-17 10:36:48 +0200

    fixed parameters and the confidence matrix format

--- Direct_Access_Image_Sample.cpp
+++ Direct_Access_Image_Sample.cpp
@@ -44,10 +44,6 @@ int** getBernsenThreshold(BYTE **image, int height, int width) {
 	int **threshold = (int**)malloc(height * sizeof(int *));
 	int **neighMax = (int**)malloc(height * sizeof(int *));
 	int **neighMin = (int**)malloc(height * sizeof(int *));
-	FILE *f1 = fopen("threshold", "w");
-	FILE *f2 = fopen("min", "w");
-	FILE *f3 = fopen("max", "w");
-	FILE *f4 = fopen("original", "w");
 	for (int i = 0; i < height; i++) {
 		threshold[i] = (int*)malloc(width * sizeof(int));
 		neighMax[i] = (int*)malloc(width * sizeof(int));
@@ -59,20 +55,8 @@ int** getBernsenThreshold(BYTE **image, int height, int width) {
 			neighMax[i][j] = getMaxNeigh(image, i, j, height, width, 30);
 			neighMin[i][j] = getMinNeigh(image, i, j, height, width, 30);
 			threshold[i][j] = (neighMax[i][j] + neighMin[i][j]) / 2;
-			/*fprintf(f1, "%d ", threshold[i][j]);
-			fprintf(f2, "%d ", neighMax[i][j]);
-			fprintf(f3, "%d ", neighMin[i][j]);
-			fprintf(f4, "%d ", image[i][j]);*/
 		}
-		/*fprintf(f1, "\n");
-		fprintf(f2, "\n");
-		fprintf(f3, "\n");
-		fprintf(f4, "\n");*/
 	}
-	/*fclose(f1);
-	fclose(f2);
-	fclose(f3);
-	fclose(f4);*/
 	return threshold;
 }
 
@@ -81,9 +65,9 @@ int** getBernsenThreshold(BYTE **image, int height, int width) {
 int _tmain(int argc, _TCHAR* argv[])
 {
     //Verify command-line usage correctness
-    if (argc != 2)
+    if (argc != 4)
     {
-        _tprintf(_T("Use: %s <Input_Image_File_Name (24BPP True-Color)>\n"), argv[0]);
+        _tprintf(_T("Use: %s <Input_Image_File_Name (24BPP True-Color)> <Output_Image_File_Name> <Confidence_Matrix_File_Name>\n"), argv[0]);
         return -1;
     }
 
@@ -120,7 +104,6 @@ int _tmain(int argc, _TCHAR* argv[])
     
     //Request direct access to image pixels in raw format
     BYTE **pDataMatrixGrayscale = NULL;
-	FILE *f = fopen("confidence", "w");
     if (pImageGrayscale->BeginDirectAccess() && (pDataMatrixGrayscale = pImageGrayscale->GetDataMatrix()) != NULL)
     {
         //If direct access is obtained get image attributes and start processing pixels
@@ -129,7 +112,9 @@ int _tmain(int argc, _TCHAR* argv[])
 
         //Create binary image
         KImage *pImageBinary = new KImage(intWidth, intHeight, 1);
-        if (pImageBinary->BeginDirectAccess())
+		//Create confidence matrix
+		KImage *pConfidenceImageBinary = new KImage(intWidth, intHeight, 8);
+		if (pImageBinary->BeginDirectAccess() && pConfidenceImageBinary->BeginDirectAccess())
         {
             int **threshold = getBernsenThreshold(pDataMatrixGrayscale, intHeight, intWidth);
             for (int y = intHeight - 1; y >= 0; y--) {
@@ -145,20 +130,23 @@ int _tmain(int argc, _TCHAR* argv[])
                     else
                         //...if closer to white, set to white
                         pImageBinary->Put1BPPPixel(x, y, true);
-
-					fprintf(f, "%d ", abs(PixelAtXY - threshold[y][x]));
+					pConfidenceImageBinary->Put8BPPPixel(x, y, abs(PixelAtXY - threshold[y][x]));
                 }
-				fprintf(f, "\n");
 			}
             //Close direct access
             pImageBinary->EndDirectAccess();
+			pConfidenceImageBinary->EndDirectAccess();
             
             //Save binarized image
-            _stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s_Black_and_White.TIF"), argv[0]);
+            _stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), argv[2]);
             pImageBinary->SaveAs(strNewFileName, SAVE_TIFF_CCITTFAX4);
+			//Save binarized confidence matrix image
+			_stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), argv[3]);
+            pConfidenceImageBinary->SaveAs(strNewFileName, SAVE_TIFF_CCITTFAX4);
 
             //Don't forget to delete the binary image
             delete pImageBinary;
+			delete pConfidenceImageBinary;
         }
         else
         {
