204332cec93397b4220cb3af19bd21bc9eb484a9
Tapus Radu <radutzp@gmail.com>
2013-12-18 01:42:30 +0200

    Added support for CL parameters and created confidence matrix

--- Direct_Access_Image_Sample.cpp
+++ Direct_Access_Image_Sample.cpp
@@ -19,56 +19,52 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    //Verify command-line usage correctness
-    if (argc != 2)
-    {
-        _tprintf(_T("Use: %s <Input_Image_File_Name (24BPP True-Color)>\n"), argv[0]);
-        return -1;
-    }
+	//Verify command-line usage correctness
+	if (argc != 4)
+	{
+		_tprintf(_T("Use: %s <Input_Image_File_Name (24BPP True-Color)> <Output_Image_File_Name> <Confidence_Matrix_File_Name>\n"), argv[0]);
+		return -1;
+	}
 
-    //Buffer for the new file names
-    TCHAR strNewFileName[0x100];
+	//Buffer for the new file names
+	TCHAR strNewFileName[0x100];
 
-    //Load and verify that input image is a True-Color one
-    KImage *pImage = new KImage(argv[1]);
-    if (pImage == NULL || !pImage->IsValid() || pImage->GetBPP() != 24)
-    {
-        _tprintf(_T("File %s does is not a valid True-Color image!"), argv[0]);
-        return -2;
-    }
-	
-    //Apply a Gaussian Blur with small radius to remove obvious noise
-    pImage->GaussianBlur(0.5);
-    _stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s_blurred.TIF"), argv[0]);
-    pImage->SaveAs(strNewFileName, SAVE_TIFF_LZW);
+	//Load and verify that input image is a True-Color one
+	KImage *pImage = new KImage(argv[1]);
+	if (pImage == NULL || !pImage->IsValid() || pImage->GetBPP() != 24)
+	{
+		_tprintf(_T("File %s does is not a valid True-Color image!"), argv[0]);
+		return -2;
+	}
 
-    //Convert to grayscale
-    KImage *pImageGrayscale = pImage->ConvertToGrayscale();
-    //Don't forget to delete the original, now useless image
-    delete pImage;
+	//Apply a Gaussian Blur with small radius to remove obvious noise
+	pImage->GaussianBlur(0.5);
 
-    //Verify conversion success...
-    if (pImageGrayscale == NULL || !pImageGrayscale->IsValid() || pImageGrayscale->GetBPP() != 8)
-    {
-        _tprintf(_T("Conversion to grayscale was not successfull!"));
-        return -3;
-    }
-    //... and save grayscale image
-    _stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s_grayscale.TIF"), argv[0]);
-    pImageGrayscale->SaveAs(strNewFileName, SAVE_TIFF_LZW);
-    
-    //Request direct access to image pixels in raw format
-    BYTE **pDataMatrixGrayscale = NULL;
-    if (pImageGrayscale->BeginDirectAccess() && (pDataMatrixGrayscale = pImageGrayscale->GetDataMatrix()) != NULL)
-    {
-        //If direct access is obtained get image attributes and start processing pixels
-        int intWidth = pImageGrayscale->GetWidth();
-        int intHeight = pImageGrayscale->GetHeight();
+	//Convert to grayscale
+	KImage *pImageGrayscale = pImage->ConvertToGrayscale();
+	//Don't forget to delete the original, now useless image
+	delete pImage;
 
-        //Create binary image
-        KImage *pImageBinary = new KImage(intWidth, intHeight, 1);
-        if (pImageBinary->BeginDirectAccess())
-        {
+	//Verify conversion success...
+	if (pImageGrayscale == NULL || !pImageGrayscale->IsValid() || pImageGrayscale->GetBPP() != 8)
+	{
+		_tprintf(_T("Conversion to grayscale was not successfull!"));
+		return -3;
+	}
+
+	//Request direct access to image pixels in raw format
+	BYTE **pDataMatrixGrayscale = NULL;
+	if (pImageGrayscale->BeginDirectAccess() && (pDataMatrixGrayscale = pImageGrayscale->GetDataMatrix()) != NULL)
+	{
+		//If direct access is obtained get image attributes and start processing pixels
+		int intWidth = pImageGrayscale->GetWidth();
+		int intHeight = pImageGrayscale->GetHeight();
+
+		//Create binary image
+		KImage *pImageBinary = new KImage(intWidth, intHeight, 1);
+		KImage *pConfidenceImage = new KImage(intWidth, intHeight, 8);
+		if (pImageBinary->BeginDirectAccess() && pConfidenceImage->BeginDirectAccess())
+		{
 			KImage *pImagePadded = pImageGrayscale->PadImage();
 			double **imageMatrix = pImagePadded->ConvertToDouble();
 			double **accMatrix = AccumulateMatrix(imageMatrix, intHeight, intWidth);
@@ -89,49 +85,56 @@ int _tmain(int argc, _TCHAR* argv[])
 					if ((double)pImageGrayscale->Get8BPPPixel(j, i) > thresholdMatrix[i][j])
 					{
 						pImageBinary->Put1BPPPixel(j, i, true);
+						pConfidenceImage->Put8BPPPixel(j, i, 0x00);
 					}
 					else
 					{
 						pImageBinary->Put1BPPPixel(j, i, false);
+						pConfidenceImage->Put8BPPPixel(j, i, 0xFF);
 					}
 				}
 			}
 
-            //Close direct access
-            pImageBinary->EndDirectAccess();
+			//Close direct access
+			pImageBinary->EndDirectAccess();
 			pImagePadded->EndDirectAccess();
-            
-            //Save binarized image
-            _stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s_Black_and_White.TIF"), argv[0]);
-            pImageBinary->SaveAs(strNewFileName, SAVE_TIFF_CCITTFAX4);
+			pConfidenceImage->EndDirectAccess();
 
-            //Don't forget to delete the binary image
-            delete pImageBinary;
+			//Save binarized image
+			_stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s.TIF"), argv[2]);
+			pImageBinary->SaveAs(strNewFileName, SAVE_TIFF_CCITTFAX4);
+
+			_stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s.TIF"), argv[3]);
+			pImageBinary->SaveAs(strNewFileName, SAVE_TIFF_LZW);
+
+			//Don't forget to delete the binary image
+			delete pImageBinary;
 			delete meanMatrix;
 			delete accMatrix;
 			delete imageMatrix;
 			delete pImagePadded;
-        }
-        else
-        {
-            _tprintf(_T("Unable to obtain direct access in binary image!"));
-            return -3;
-        }
+			delete pConfidenceImage;
+		}
+		else
+		{
+			_tprintf(_T("Unable to obtain direct access in binary image!"));
+			return -3;
+		}
 
-        //Close direct access
-        pImageGrayscale->EndDirectAccess();
-    }
-    else
-    {
-        _tprintf(_T("Unable to obtain direct access in grayscale image!"));
-        return -4;
-    }
+		//Close direct access
+		pImageGrayscale->EndDirectAccess();
+	}
+	else
+	{
+		_tprintf(_T("Unable to obtain direct access in grayscale image!"));
+		return -4;
+	}
 
-    //Don't forget to delete the grayscale image
-    delete pImageGrayscale;
+	//Don't forget to delete the grayscale image
+	delete pImageGrayscale;
 
-    //Return with success
-    return 0;
+	//Return with success
+	return 0;
 }
 //===========================================================================
 //===========================================================================
